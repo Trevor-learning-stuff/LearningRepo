714c8f719e85d2f8b3cd1ee452ac29a661e82f47
Trevor Campbell <trevor.campbell@infusionsoft.com>
2016-04-25 10:15:52 -0700

    Date and Date tester

--- src/Date.java
+++ src/Date.java
@@ -1,5 +1,25 @@
 /**
- * Created by trevor.campbell on 3/28/16.
+ * Programmer: Trevor Campbell             CSC110AB Module 5 Part 2
+ * Date: April 11, 2016
+ * Description: This creates a custom date object that a programmer can use
+ *
+ * Input: none
+ * Output: error for invalid dates
+ *
+ * Pseudocode for this solution:
+ * Begin
+ * can be constructed in 3 ways
+ *      default - defaults to jan. 1, 2000
+ *      set day, month, year
+ *      set from another Date object
+ * Methods for getting each of the Instance variables - month, day, year
+ * Methods for setting each of the Instance variables - month, day, year
+ * toString Method - Any time the date is printed, it will be formatted as JAN 1, 2000
+ * Equals Method - check if the Date object is equal to another
+ * getDayNumber Method - returns the day of the year for the current set date
+ * addDays Method - add or subtract days and return the proper date
+ * compareTo Method - returns -1 if date is less than another, returns 0 if equal, returns 1 if greater
+ * End
  */
 public class Date {
 
@@ -21,7 +41,7 @@ public class Date {
     public static final int DEC = 12;
 
     public Date(){
-        month = 1;
+        month = JAN;
         day = 1;
         year = 2000;
     }
@@ -84,28 +104,123 @@ public class Date {
         return other.getDay() == this.getDay() && other.getMonth() == this.getMonth() && other.getYear() == this.getYear();
     }
 
-    // TODO: 4/1/16 do these
     public int getDayNumber() {
-        return (int)30.6001*(month+1);
+        int numberDays = 0;
+
+        if (isLeapYear()) {
+            switch (month) {
+                case JAN:
+                    numberDays = day;
+                    break;
+                case FEB:
+                    numberDays = 31 + day;
+                    break;
+                case MAR:
+                    numberDays = 60 + day;
+                    break;
+                case APR:
+                    numberDays = 91 + day;
+                    break;
+                case MAY:
+                    numberDays = 121 + day;
+                    break;
+                case JUN:
+                    numberDays = 152 + day;
+                    break;
+                case JUL:
+                    numberDays = 182 + day;
+                    break;
+                case AUG:
+                    numberDays = 213 + day;
+                    break;
+                case SEP:
+                    numberDays = 244 + day;
+                    break;
+                case OCT:
+                    numberDays = 274 + day;
+                    break;
+                case NOV:
+                    numberDays = 305 + day;
+                    break;
+                case DEC:
+                    numberDays = 335 + day;
+                    break;
+            }
+        }
+
+        if (!isLeapYear()) {
+
+            switch (month) {
+                case JAN:
+                    numberDays = day;
+                    break;
+                case FEB:
+                    numberDays = 31 + day;
+                    break;
+                case MAR:
+                    numberDays = 59 + day;
+                    break;
+                case APR:
+                    numberDays = 90 + day;
+                    break;
+                case MAY:
+                    numberDays = 120 + day;
+                    break;
+                case JUN:
+                    numberDays = 151 + day;
+                    break;
+                case JUL:
+                    numberDays = 181 + day;
+                    break;
+                case AUG:
+                    numberDays = 212 + day;
+                    break;
+                case SEP:
+                    numberDays = 243 + day;
+                    break;
+                case OCT:
+                    numberDays = 273 + day;
+                    break;
+                case NOV:
+                    numberDays = 304 + day;
+                    break;
+                case DEC:
+                    numberDays = 334 + day;
+                    break;
+            }
+        }
+
+        return numberDays;
     }
 
     public void addDays(int amt) {
 
+        if (isLeapYear()) {
+            if (amt >= 366 || amt <= -366) {
+                year += amt / 366;
+                this.calculateMonthAndDay(this.getDayNumber() + amt - 366);
+            }else{
+                this.calculateMonthAndDay(getDayNumber() + amt);
+            }
+        }else {
+            if (amt >= 365 || amt <= -365) {
+                year += amt / 365;
+                this.calculateMonthAndDay(this.getDayNumber() + amt - 365);
+            }else{
+                this.calculateMonthAndDay(getDayNumber() + amt);
+            }
+        }
     }
 
     public int compareTo(Date other) {
-        if (this.day < other.getDay() || this.month < other.getMonth() || this.year < other.getMonth()) {
+        if (this.day < other.getDay()|| this.month < other.getMonth() || this.year < other.getYear()) {
             return -1;
-        }else if (this.day == other.getDay() || this.month == other.getMonth() || this.year == other.getMonth()) {
+        }else if (this.day == other.getDay() && this.month == other.getMonth() && this.year == other.getYear()) {
             return 0;
         }else{
             return 1;
         }
     }
-// TODO: 4/1/16 do this one too
-    /*public int getDaysApart(Date other) {
-
-    }*/
 
     private boolean isValidDay(int aDay) {
         switch (month) {
@@ -148,7 +263,7 @@ public class Date {
         }
     }
 
-    public boolean isValidYear(int aYear) {
+    private boolean isValidYear(int aYear) {
         if (aYear > 0) {
             return true;
         }else{
@@ -161,32 +276,153 @@ public class Date {
     }
 
     private String convertMonthToStringName(int aMonth) {
+        String month = "";
+
         switch (aMonth) {
             case 1:
-                return "JAN";
+                month = "JAN";
+                break;
             case 2:
-                return "FEB";
+                month = "FEB";
+                break;
             case 3:
-                return "MAR";
+                month = "MAR";
+                break;
             case 4:
-                return "APR";
+                month = "APR";
+                break;
             case 5:
-                return "MAY";
+                month = "MAY";
+                break;
             case 6:
-                return "JUN";
+                month = "JUN";
+                break;
             case 7:
-                return "JUL";
+                month = "JUL";
+                break;
             case 8:
-                return "AUG";
+                month = "AUG";
+                break;
             case 9:
-                return "SEP";
+                month = "SEP";
+                break;
             case 10:
-                return "OCT";
+                month = "OCT";
+                break;
             case 11:
-                return "NOV";
+                month = "NOV";
+                break;
             case 12:
-                return "DEC";
+                month = "DEC";
+                break;
         }
-        return null;
+        return month;
+    }
+
+    private void calculateMonthAndDay(int dayOfYear) {
+
+        if(!isLeapYear()) {
+            if (dayOfYear >= 1 && dayOfYear <= 31) {
+                this.month = JAN;
+                this.day = dayOfYear;
+
+            } else if (dayOfYear >= 32 && dayOfYear <= 59) {
+                this.month = FEB;
+                this.day = 28 - (59 - dayOfYear);
+
+            } else if (dayOfYear >= 60 && dayOfYear <= 90) {
+                this.month = MAR;
+                this.day = 31 - (90 - dayOfYear);
+
+            } else if (dayOfYear >= 91 && dayOfYear <= 120) {
+                this.month = APR;
+                this.day = 30 - (120 - dayOfYear);
+
+            } else if (dayOfYear >= 121 && dayOfYear <= 151) {
+                this.month = MAY;
+                this.day = 31 - (151 - dayOfYear);
+
+            } else if (dayOfYear >= 152 && dayOfYear <= 181) {
+                this.month = JUN;
+                this.day = 30 - (181 - dayOfYear);
+
+            } else if (dayOfYear >= 182 && dayOfYear <= 212) {
+                this.month = JUL;
+                this.day = 31 - (212 - dayOfYear);
+
+            } else if (dayOfYear >= 213 && dayOfYear <= 243) {
+                this.month = AUG;
+                this.day = 31 - (243 - dayOfYear);
+
+            } else if (dayOfYear >= 244 && dayOfYear <= 273) {
+                this.month = SEP;
+                this.day = 30 - (273 - dayOfYear);
+
+            } else if (dayOfYear >= 274 && dayOfYear <= 304) {
+                this.month = OCT;
+                this.day = 31 - (304 - dayOfYear);
+
+            } else if (dayOfYear >= 205 && dayOfYear <= 334) {
+                this.month = NOV;
+                this.day = 30 - (334 - dayOfYear);
+
+            } else if (dayOfYear >= 335 && dayOfYear <= 365) {
+                this.month = DEC;
+                this.day = 31 - (365 - dayOfYear);
+
+            }
+        }
+
+        if (isLeapYear()) {
+            if (dayOfYear >= 1 && dayOfYear <= 31) {
+                this.month = JAN;
+                this.day = dayOfYear;
+
+            } else if (dayOfYear >= 32 && dayOfYear <= 60) {
+                this.month = FEB;
+                this.day = 29 - (60 - dayOfYear);
+
+            } else if (dayOfYear >= 61 && dayOfYear <= 91) {
+                this.month = MAR;
+                this.day = 31 - (91 - dayOfYear);
+
+            } else if (dayOfYear >= 92 && dayOfYear <= 121) {
+                this.month = APR;
+                this.day = 30 - (121 - dayOfYear);
+
+            } else if (dayOfYear >= 122 && dayOfYear <= 152) {
+                this.month = MAY;
+                this.day = 31 - (152 - dayOfYear);
+
+            } else if (dayOfYear >= 153 && dayOfYear <= 182) {
+                this.month = JUN;
+                this.day = 30 - (182 - dayOfYear);
+
+            } else if (dayOfYear >= 183 && dayOfYear <= 213) {
+                this.month = JUL;
+                this.day = 31 - (213 - dayOfYear);
+
+            } else if (dayOfYear >= 214 && dayOfYear <= 244) {
+                this.month = AUG;
+                this.day = 31 - (243 - dayOfYear);
+
+            } else if (dayOfYear >= 245 && dayOfYear <= 274) {
+                this.month = SEP;
+                this.day = 30 - (274 - dayOfYear);
+
+            } else if (dayOfYear >= 275 && dayOfYear <= 305) {
+                this.month = OCT;
+                this.day = 31 - (305 - dayOfYear);
+
+            } else if (dayOfYear >= 206 && dayOfYear <= 335) {
+                this.month = NOV;
+                this.day = 30 - (334 - dayOfYear);
+
+            } else if (dayOfYear >= 336 && dayOfYear <= 366) {
+                this.month = DEC;
+                this.day = 31 - (366 - dayOfYear);
+
+            }
+        }
     }
 }
--- src/DateTester.java
+++ src/DateTester.java
@@ -23,7 +23,7 @@ public class DateTester {
     }
 
     @Test
-    public void testSetDaysMethodWorks() {
+    public void testSetDaysMethodSetsTheDay() {
         Date date = new Date();
         date.setDay(15);
 
@@ -31,9 +31,131 @@ public class DateTester {
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class)
-    public void testThrowIfDayIsInvalid() {
+    public void testThrowExceptionIfDayIsInvalid() {
         Date date = new Date(1, 31, 2010);
         date.setDay(45);
+    }
+
+    @Test
+    public void testSetMonthSetsTheMonth() {
+        Date date = new Date(1, 1, 2016);
+        date.setMonth(12);
+
+        Assert.assertEquals(date.getMonth(), 12, "The month should be set to 12");
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testThrowExceptionIfMonthIsInvalid() {
+        Date date = new Date(1, 31, 2010);
+        date.setMonth(15);
+    }
+
+    @Test
+    public void testSetYearSetsTheYear() {
+        Date date = new Date(1, 1, 2016);
+        date.setYear(2019);
+
+        Assert.assertEquals(date.getYear(), 2019, "The year should be set to 2019");
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testThrowExceptionIfYearIsInvalid() {
+        Date date = new Date(1, 31, 2010);
+        date.setYear(-10);
+    }
+
+    @Test
+    public void testCompareToIsLessThanAnother() {
+        Date earlierDate = new Date(1, 1, 2016);
+        Date laterDate = new Date(2, 1, 2016);
+
+        Assert.assertTrue(earlierDate.compareTo(laterDate) == -1, "The value should be -1");
+    }
+
+    @Test
+    public void testCompareToIsGreaterThanAnother() {
+        Date earlierDate = new Date(2, 1, 2016);
+        Date laterDate = new Date(1, 1, 2016);
+
+        Assert.assertTrue(earlierDate.compareTo(laterDate) == 1, "The value should be 1");
+    }
+
+    @Test
+    public void testCompareToIsEqualToAnother() {
+        Date earlierDate = new Date(2, 1, 2016);
+        Date laterDate = new Date(2, 1, 2016);
+
+        Assert.assertTrue(earlierDate.compareTo(laterDate) == 0, "The value should be 0");
+    }
+
+    @Test
+    public void testToStringFormatsProperly() {
+        Date date = new Date(3, 1, 2015);
+        String stringDate = date.toString();
+
+        Assert.assertTrue(stringDate.equals("MAR-1-2015"), "the date should be in the format of \"MAR-1-2015\"");
+    }
+
+    @Test
+    public void testGetDayNumberReturnsTheCorrectDateLeapYear() {
+        Date date = new Date(5, 1, 2016);
+
+        Assert.assertEquals(date.getDayNumber(), 122, "The day of the year should be 122");
+    }
+
+    @Test
+    public void testGetDayNumberReturnsTheCorrectDate() {
+        Date date = new Date(5, 1, 2015);
+
+        Assert.assertEquals(date.getDayNumber(), 121, "The day of the year should be 121");
+    }
+
+    @Test
+    public void testAddDaysLeapYearShortTerm() {
+        Date date1 = new Date(2, 28, 2016);
+        Date date2 = new Date(3, 1, 2016);
+        date1.addDays(2);
+
+        String stringDate1 = String.valueOf(date1);
+        String stringDate2 = String.valueOf(date2);
+
+        Assert.assertEquals(stringDate1, stringDate2, "The date should be set to MAR-1-2016" );
+    }
+
+    @Test
+    public void testAddDaysNotLeapYearShortTerm() {
+        Date date1 = new Date(3, 1, 2015);
+        Date date2 = new Date(2, 28, 2015);
+        date1.addDays(-1);
+
+        String stringDate1 = String.valueOf(date1);
+        String stringDate2 = String.valueOf(date2);
+
+        Assert.assertEquals(stringDate1, stringDate2, "The date should be set to MAR-1-2015" );
+    }
+
+    @Test
+    public void testAddDaysNotLeapYearLongTerm() {
+        Date date1 = new Date(3, 1, 2015);
+        Date date2 = new Date(3, 1, 2016);
+        date1.addDays(365);
+
+        String stringDate1 = String.valueOf(date1);
+        String stringDate2 = String.valueOf(date2);
+
+        Assert.assertEquals(stringDate1, stringDate2, "The date should be set to MAR-1-2016" );
+    }
+
+    @Test
+    public void testAddDaysLeapYearLongTerm() {
+        Date date1 = new Date(3, 1, 2016);
+        Date date2 = new Date(3, 1, 2015);
+        date1.addDays(-366);
+
+        String stringDate1 = String.valueOf(date1);
+        String stringDate2 = String.valueOf(date2);
+
+        Assert.assertEquals(stringDate1, stringDate2, "The date should be set to MAR-1-2015" );
     }
 
 
